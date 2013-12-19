d17bbfb24dea54438794bc62f6cf812cf289f3f7
zhaohuan <byzhaohuan@gmail.com>
2013-12-19 21:34:36 +0800

    query_parse support natural language

--- search/query_parser.py
+++ search/query_parser.py
@@ -8,6 +8,8 @@ sys.path.append('../')
 
 from ltp_service import ltpservice
 
+from tools.generate_adjs_from_dependency_res import parse_XML
+
 module_dir = os.path.dirname(__file__)  # get current directory
 
 #(field, weight)
@@ -22,6 +24,12 @@ non_person_fields_weight = [('title', '10.0'),
                             #('user_tags', '1.0'),
                             ('summary', '5.0')
                             ]
+
+def unicode_to_str(raw_str):
+    if isinstance(raw_str, str):
+        return raw_str
+    elif isinstance(raw_str, unicode):
+        return raw_str.encode('utf8')
 
 class Parser:
 
@@ -64,25 +72,43 @@ class Parser:
             return ' '.join(lines)
 
 
-        if term in self.person_terms and term in self.non_person_terms:
-            query_str = generate_query_by_fields(term, person_fields_weight + non_person_fields_weight)
-        elif term in self.person_terms:
-            query_str = generate_query_by_fields(term, person_fields_weight)
-        elif term in self.non_person_terms:
-            query_str = generate_query_by_fields(term, non_person_fields_weight)
-        else:
-            query_str = generate_query_by_fields(term, person_fields_weight + non_person_fields_weight)
-        return query_str
+        try:
+            if term in self.person_terms and term in self.non_person_terms:
+                query_str = generate_query_by_fields(term, person_fields_weight + non_person_fields_weight)
+            elif term in self.person_terms:
+                query_str = generate_query_by_fields(term, person_fields_weight)
+            elif term in self.non_person_terms:
+                query_str = generate_query_by_fields(term, non_person_fields_weight)
+            else:
+                #这个时候很有可能是一句话，就引入ltp进行分词
+                #import pdb;pdb.set_trace()
+                import time
+                start = time.time()
+                ltp_res = self.ltp_client.analysis(unicode_to_str(term), ltpservice.LTPOption.PARSER)
+                #print ltp_res.tostring()
+                adjs = parse_XML(ltp_res.tostring(), 1)#直接取出形容词即可
+                #能找出形容词则生成形容词域，否则直接返回term
+                print 'get adjs cost %.2fs' % (time.time() - start)
+                query_str = ' '.join(['adjs:%s' % a for a in adjs]) if adjs else term
+            return query_str
+        except Exception as e:
+            print e
+            return term
 
     def test_ltp(self, raw_str):
         result = self.ltp_client.analysis(raw_str, ltpservice.LTPOption.PARSER)
         print result.tostring()
 
-def test_parser():
+def test_parser(raw_str):
     parser = Parser()
     #term = u'张艺谋 章子怡'
     #print term in parser.person_terms
-    parser.test_ltp('很多人觉得剧情很矫情')
+    #parser.test_ltp('很多人觉得剧情很矫情')
+    print parser.parse(raw_str)
 
 if __name__ == '__main__':
-    test_parser()
+    if len(sys.argv) > 1:
+        raw_str = sys.argv[1]
+    else:
+        raw_str = u'我想看快乐的电影'
+    test_parser(raw_str)
