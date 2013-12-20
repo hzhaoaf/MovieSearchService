d4abc5429a2d5016774885d4f4b754a6c200cc43
zhaohuan <byzhaohuan@gmail.com>
2013-12-21 01:09:37 +0800

    modify query_parser
    1, 任意一个term，默认都有title, directors, casts三个域的搜索；
    2, 对于aka，tags等其他域，一旦击中，则会添加到生成的query str中，并且权重较高;
    3, 考虑击中2中的域的时候，降低1中3个域的权重? 有待验证

--- search/query_parser.py
+++ search/query_parser.py
@@ -2,6 +2,9 @@
 
 '''
     这个模块对用户输入的词进行解析，生成适合我们的query形式
+    默认生成directors，casts，title三个域的search。
+    剩下的，击中了某个域，就对应增加那个域，并按照预定的权重生成query
+    国外中的人名有中文“·”，所以需要将这个拆分开来
 '''
 import os, sys
 sys.path.append('../')
@@ -13,17 +16,19 @@ from tools.xml_processor import parse_XML
 module_dir = os.path.dirname(__file__)  # get current directory
 
 #(field, weight)
-person_fields_weight = [('directors', '10.0'),
-                        ('casts', '5.0'),
-                        #('writers', '1.0')
-                        ]
-non_person_fields_weight = [('title', '10.0'),
-                            ('original_title', '10.0'),
-                            ('aka', '7.0'),
-                            ('countries', '0.5'),
-                            #('user_tags', '1.0'),
-                            ('summary', '5.0')
-                            ]
+#每个query term默认都有这几个search field，以及权重
+basic_fields_weight = {'directors': '10.0',
+                       'casts': '10.0',
+                       'title': '10.0',
+                       'summary': '2.0'
+                       }
+#一旦检查到该term也有custom_fields中的type，需要增加该域的搜索
+custom_fields_weight = {'original_title': '10.0',
+                        'aka': '10.0',
+                        'countries': '15.0',
+                        'user_tags': '15.0',
+                        'year': '20.0',
+                        }
 
 def unicode_to_str(raw_str):
     if isinstance(raw_str, str):
@@ -38,19 +43,23 @@ class Parser:
         self.ltp_client = ltpservice.LTPService("%s:%s" % ('zhaoh@cslt.riit.tsinghua.edu.cn', '6Ts8ZDJK'))
 
     def load_film_terms(self):
-        '''导入离线生成的terms
+        '''导入离线生成的terms, 用来判断term的基本从属情况
 
-        导入从数据库成生成全部的人名和非人名的术语集合，用来判断用户的术语所属基本类型
+        导入从数据库成生成全部术语集合，用来判断用户的术语所属基本类型
+        生成一个子典型数据，key为term，value为list，保存该term从属的全部类型（可能人名会出现在导演，演员，title，user_tags中）
 
         '''
         fields_terms_path = os.path.join(module_dir, 'fields_terms.txt')
         lines = open(fields_terms_path, 'r').readlines()
         terms = [tuple(l.strip().split('<￥>')) for l in lines if l]
-        person_terms_types = [r[0] for r in person_fields_weight]
-        non_person_terms_types = [r[0] for r in non_person_fields_weight]
+        self.term_types = {}
+        for term_str, type_ in terms:
+            #国外的中文译名中存在·，需要split之后，以单个人名来搜索
+            terms = term_str.split('·')
+            for term in terms:
+                self.term_types.setdefault(term, []).append(type_)
 
-        self.person_terms = {r[0].decode('utf8'): r[1] for r in terms if r[1] in person_terms_types}
-        self.non_person_terms = {r[0].decode('utf8'): r[1] for r in terms if r[1] in non_person_terms_types}
+
 
     def parse(self, raw_str):
         terms = raw_str.split()
@@ -62,23 +71,23 @@ class Parser:
     def parse_term(self, term):
         ''' 解析单个term
 
-        基于person_terms和non_person_terms判断该term的基本情况，然后生成不同类型的query_str
-
+            每个term默认输入title, casts, directors三个域，其他类型一旦击中，都需要增加该域，以及预设的权重
         '''
 
         def generate_query_by_fields(term, field_info):
-            lines = ['%s:%s^%s' % (r[0], term, r[1]) for r in field_info]
+            lines = ['%s:%s^%s ' % (f, term, w) for f, w in field_info.items()]
             #lines = ['%s:%s' % (r[0], term) for r in field_info]
-            return ' '.join(lines)
-
+            return ''.join(lines)
 
         try:
-            if term in self.person_terms and term in self.non_person_terms:
-                query_str = generate_query_by_fields(term, person_fields_weight + non_person_fields_weight)
-            elif term in self.person_terms:
-                query_str = generate_query_by_fields(term, person_fields_weight)
-            elif term in self.non_person_terms:
-                query_str = generate_query_by_fields(term, non_person_fields_weight)
+            query_str = generate_query_by_fields(term, basic_fields_weight)
+            if term in self.term_types:
+                types = self.term_types[term]
+                custom_types = [t for t in types if t not in basic_fields_weight.keys()]
+                if custom_types:
+                    need_custom_fields = {ct: custom_fields_weight.get(ct, '1.0') for ct in custom_types}
+                    query_str += generate_query_by_fields(term, need_custom_fields)
+
             else:
                 #这个时候很有可能是一句话，就引入ltp进行分词
                 #import pdb;pdb.set_trace()
@@ -91,8 +100,6 @@ class Parser:
                 #print 'get adjs cost %.2fs' % (time.time() - start)
                 if adjs:
                     query_str = ' '.join(['adjs:%s' % a for a in adjs])
-                else:
-                    query_str = generate_query_by_fields(term, person_fields_weight + non_person_fields_weight)
             return query_str
         except Exception as e:
             print e
