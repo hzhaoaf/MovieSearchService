5ecf1b2df51401777cca639b0259568da0bf9487
RioDream <ucas.li.ao@gmail.com>
2014-01-02 11:02:46 +0800

    rt

--- search/test2.py
+++ search/test2.py
@@ -1,2 +1,16 @@
 import test
+import SearchMysql_v3 as SearchMysql
 
+
+
+
+VMEnv=SearchMysql.getVMEnv()
+if VMEnv:
+	print VMEnv.attachCurrentThread()
+else:
+	SearchMysql.initJvm()
+
+
+debug = str(type(VMEnv))
+
+print debug
